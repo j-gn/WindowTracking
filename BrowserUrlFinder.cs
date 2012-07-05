dcf412edbbe25c6e6603f4715da6b6adea5d1653
Johannes <substans@gmail.com>
2012-07-06 00:16:56 +0200

    new stuff

---
+++ BrowserUrlFinder.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows;
+using System.Windows.Automation;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+namespace WindowTracking
+{
+    /// <summary>
+    /// code retrieved from http://stackoverflow.com/questions/5317642/retrieve-current-url-from-c-sharp-windows-form
+    /// </summary>
+    class BrowserUrlFinder
+    {
+        public static string GetChromeUrl(Process process) {
+            if (process == null)
+                throw new ArgumentNullException("process");
+
+            if (process.MainWindowHandle == IntPtr.Zero)
+                return null;
+
+            AutomationElement element = AutomationElement.FromHandle(process.MainWindowHandle);
+            if (element == null)
+                return null;
+
+            AutomationElement edit = element.FindFirst(TreeScope.Children, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Edit));
+            return ((ValuePattern)edit.GetCurrentPattern(ValuePattern.Pattern)).Current.Value as string;
+        }
+
+        public static string GetInternetExplorerUrl(Process process) {
+            if (process == null)
+                throw new ArgumentNullException("process");
+
+            if (process.MainWindowHandle == IntPtr.Zero)
+                return null;
+
+            AutomationElement element = AutomationElement.FromHandle(process.MainWindowHandle);
+            if (element == null)
+                return null;
+
+            AutomationElement rebar = element.FindFirst(TreeScope.Children, new PropertyCondition(AutomationElement.ClassNameProperty, "ReBarWindow32"));
+            if (rebar == null)
+                return null;
+
+            AutomationElement edit = rebar.FindFirst(TreeScope.Subtree, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Edit));
+
+            return ((ValuePattern)edit.GetCurrentPattern(ValuePattern.Pattern)).Current.Value as string;
+        }
+
+        public static string GetFirefoxUrl(Process process) {
+            if (process == null)
+                throw new ArgumentNullException("process");
+
+            if (process.MainWindowHandle == IntPtr.Zero)
+                return null;
+
+            AutomationElement element = AutomationElement.FromHandle(process.MainWindowHandle);
+            if (element == null)
+                return null;
+
+            AutomationElement doc = element.FindFirst(TreeScope.Subtree, new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Document));
+            if (doc == null)
+                return null;
+
+            return ((ValuePattern)doc.GetCurrentPattern(ValuePattern.Pattern)).Current.Value as string;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace WindowTracking
+{
+    class Program
+    {      
+        /// <summary>Gives you the curret foreground window handle</summary>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern IntPtr GetForegroundWindow();
+        /// <param name="hWnd">window handle</param>
+        /// <param name="lpdwProcessId">gives you the process id </param>
+        /// <returns>might return the thread id</returns>
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+//        [DllImport("user32.dll")]
+//        static extern uint GetWindowThreadProcessId(IntPtr hWnd, IntPtr ProcessId);
+
+
+        static void Main(string[] args) {
+            IntPtr currentWindowHandle = IntPtr.Zero;
+            IntPtr lastWindowHandle = IntPtr.Zero;
+            while (true) {
+                currentWindowHandle = GetForegroundWindow();
+                if (currentWindowHandle.ToInt32() != lastWindowHandle.ToInt32()) {
+                    lastWindowHandle = currentWindowHandle;
+                    uint outPid;
+                    GetWindowThreadProcessId(currentWindowHandle, out outPid);
+                    int pID =(int)outPid;
+                    try {
+                        Process p = Process.GetProcessById(pID);
+                        Console.WriteLine("focus on: [" + p.ProcessName + "] pID: " + pID);
+                        switch(p.ProcessName.ToLower()){
+                            case "chrome":
+                                Console.WriteLine("URL:" + BrowserUrlFinder.GetChromeUrl(p));
+                            break;
+                            case "firefox":
+                                Console.WriteLine("URL:" + BrowserUrlFinder.GetFirefoxUrl(p));
+                            break;
+                            default:
+                            break;
+                        }
+                    }
+                    catch {
+                        Console.WriteLine("no process with id" + pID);
+                    }
+                }
+                Thread.Sleep(250);
+            }
+        }
+    }
+}
