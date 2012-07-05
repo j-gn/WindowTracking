93f868f48dc254dff05f21d440236f86f5549be0
Johannes <substans@gmail.com>
2012-07-06 01:05:24 +0200

    Added user awareness

---
+++ WindowsHookHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace InputTrackingExample
+{
+    public class WindowsHookHelper
+    {
+        public delegate IntPtr HookDelegate(
+            Int32 Code, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("User32.dll")]
+        public static extern IntPtr CallNextHookEx(
+            IntPtr hHook, Int32 nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("User32.dll")]
+        public static extern IntPtr UnhookWindowsHookEx(IntPtr hHook);
+
+
+        [DllImport("User32.dll")]
+        public static extern IntPtr SetWindowsHookEx(
+            Int32 idHook, HookDelegate lpfn, IntPtr hmod, 
+            Int32 dwThreadId);
+    }
+}
